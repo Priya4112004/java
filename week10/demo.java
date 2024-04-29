b31ee2e0313d130152236d444d9d38ec42be6fee
Priya4112004 <158718706+Priya4112004@users.noreply.github.com>
2024-04-29 18:20:02 +0530

    Create demo.java

---
+++ week10/demo.java
@@ -0,0 +1,13 @@
+import java.awt.*;
+import java.applet.*;
+/*
+<applet code="demo.class" width="1000" height="1000"> </applet>
+*/
+public class demo extends Applet {
+public void paint(Graphics g) {
+g.drawLine(20,40,100,300);
+}
+}
+
+
+
